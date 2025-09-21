3011124c1b6df6d492d179f2625eb006879b8930
web-coder <yasher18@bk.ru>
2025-09-22 00:43:11 +0300

    сделал базовую настройку логики прилодения

--- client/src/stores/counter.js
+++ client/src/stores/counter.js
@@ -1,12 +1,33 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useCounterStore = defineStore('counter', () => {
-  const count = ref(0)
-  const doubleCount = computed(() => count.value * 2)
-  function increment() {
-    count.value++
-  }
+// export const useCounterStore = defineStore('counter', () => {
+//   const count = ref(0)
+//   const doubleCount = computed(() => count.value * 2)
+//   function increment() {
+//     count.value++
+//   }
 
-  return { count, doubleCount, increment }
+//   return { count, doubleCount, increment }
+// })
+
+
+export const useRowsStore = defineStore('rows', () => {
+
+    const rows = ref([])
+    const types = ['Обычный', 'LDAP']
+
+    function addNewRow() {
+      rows.value.push({
+        name: "",
+        type: types[0],
+        login: "",
+        password: "",
+        arrayErrors: [],
+        namesSplitArray: []
+      })
+    }
+
+
+    return { rows, types, addNewRow }
 })
--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -1,14 +1,67 @@
 <template>
+  <div class="container mt-5">
+    <h3>{{ title }}</h3>
+    <button class="btn btn-success" @click="addNewRows()">Создать новую метку</button>
+  </div>
+
+  <div class="container mt-5">
+    <div class="table-responsive">
+      <table class="table table-bordered">
+        <thead class="bg-success text-light">
+          <tr>
+            <th>Метка</th>
+            <th>Тип</th>
+            <th>Логин</th>
+            <th>Пароль</th>
+            <th>Действие</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="(item, index) in allRows">
+            <td>{{ item.name }}</td>
+            <td>{{ item.type }}</td>
+            <td>{{ item.login }}</td>
+            <td>{{ item.password }}</td>
+            <td>
+              <button class="btn btn-outline-success">Удалить</button>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  </div>
 
 </template>
 
 
 <script>
 
+    import { useRowsStore } from '@/stores/counter'
+    import { computed } from 'vue'
+
     export default {
+
+      setup() {
+        const rows = useRowsStore()
+
+        const allTypes = computed(() => rows.types)
+        const allRows = computed(() => rows.rows)
+
+        const addNewRows = () => {
+          rows.addNewRow()
+        }
+
+        return {
+          allTypes,
+          allRows,
+          addNewRows
+        }
+
+      },
+
       data() {
         return {
-          title: "Создать новую метку"
+          title: "Создать новую метку",
         }
       }
     }
